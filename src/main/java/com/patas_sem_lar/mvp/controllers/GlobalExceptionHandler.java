095c927fb9b8b3912e47deb089903bea68168450
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-03-04 11:54:46 +0000

    adiciona handler para mandar mensagem representativa de erro para cliente
    em caso de email ou senha invalidos, retorna erro genérico (evitando
    informar qual campo esta incorreto) - boas práticas de segurança

--- src/main/java/com/patas_sem_lar/mvp/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/patas_sem_lar/mvp/controllers/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.patas_sem_lar.mvp.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,5 +22,12 @@ public class GlobalExceptionHandler {
                 .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
 
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(InternalAuthenticationServiceException.class)
+    public ResponseEntity<Map<String, String>> handleBadCredentials(InternalAuthenticationServiceException ex) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(Map.of("error", "Email ou senha incorretos"));
     }
 }
