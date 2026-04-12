6e07853d6d26c99e7820c2033d13b81595672eae
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-04-12 12:33:06 +0100

    feat: cria `mixins.py` definindo atributos compartilháveis entre diversas tabelas

---
+++ app/models/mixins.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timezone
+from typing import Optional
+
+from sqlalchemy import DateTime, String, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from ulid import ULID
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class TimestampMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=lambda: datetime.now(timezone.utc),
+        nullable=False,
+    )
+    deleted_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True),
+        nullable=True,
+        default=None,
+    )
+
+
+class ULIDMixin:
+    id: Mapped[str] = mapped_column(
+        String(26),
+        primary_key=True,
+        default=lambda: str(ULID()),
+    )
