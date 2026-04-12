0356968d8ad2397c75deca539a729faf834e09ee
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-04-12 12:36:17 +0100

    feat: adiciona arquivo `base` para alembic centralizar logica

---
+++ app/db/base.py
@@ -0,0 +1,7 @@
+from app.models.mixins import Base  # noqa: F401
+from app.models.usuario import Usuario  # noqa: F401
+from app.models.loja import Loja  # noqa: F401
+from app.models.profissional import Profissional  # noqa: F401
+from app.models.servico import Servico  # noqa: F401
+from app.models.horario_trabalho import HorarioTrabalho  # noqa: F401
+from app.models.agendamento import Agendamento  # noqa: F401
