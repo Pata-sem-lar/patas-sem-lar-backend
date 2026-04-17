ec13ec0833580f9f63ca8c4b8036210ed35a18a1
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-04-17 21:07:03 +0100

    build: add config variable to handle future production issue
    Changes `echo=True` to `echo=settings.env == "development"`

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
+    env: str = "development"
     database_url: str
     database_url_test: str
     jwt_secret: str
--- app/db/session.py
+++ app/db/session.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 
 from app.core.config import settings
 
-engine = create_async_engine(settings.database_url, echo=True)
+engine = create_async_engine(settings.database_url, echo=settings.env == "development")
 
 SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
